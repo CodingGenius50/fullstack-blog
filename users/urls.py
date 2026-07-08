47d4bb78c9880fe03c5dbe17afb1dcfc4f2493a8
md.abdul hakim <ahakim2325@gmail.com>
2026-07-09 01:11:15 +0600

    Add profile API

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,11 @@
 
 
 from django.urls import path
-from .views import RegisterView
+
+from .views import RegisterView, ProfileView
 
 urlpatterns = [
-    path('register/', RegisterView.as_view()),
+   
+    path("register/", RegisterView.as_view()),
+    path("profile/", ProfileView.as_view()),
 ]
